a41f48fd7994a80cdb3d3a7575057afb0df0330c
Matteo Papaianni <papaiannimatteo746@gmail.com>
2022-04-21 12:53:19 +0200

    Esercizi di ieri aggiornati con il metodo delle funzioni

---
+++ Esercizi typescript/Ex_bisestile(IF).ts
@@ -0,0 +1,28 @@
+/*
+Si scriva un programma che preso in input un anno, stampi a se questo e` bisestile o meno.
+Un anno si dice bisestile SE e SOLO SE :
+    E` divisibile per 400
+    E` divisibile per 4 ma non per 100
+Input : 1600
+Output : "l'anno e` bisestile";
+Input : 1601
+Otuput: "L'anno non e` bisestile";
+Input : 1696
+Otuput: "L'anno e` bisestile";
+*/
+
+//Soluzione:
+
+function bisestile(number1:number):string {
+
+    if (number1%400==0 || number1%4==0 && number1%100!=0) {
+    console.log("L'anno è bisestile");
+    }
+    else{
+    console.log("L'anno non è bisestile");
+    }
+
+}
+
+let number1:number =prompt("Inserisci l'anno");
+bisestile(number1);
---
+++ Esercizi typescript/Ex_dataNascita(IF).ts
@@ -0,0 +1,20 @@
+/*
+Si scriva un programma che preso in input l'anno di nascita, stampi a schermo l'eta`:
+Input : 1992
+Output : "Hai 30 anni";
+Input : 2010
+Otuput: "Hai 12 anni";
+*/
+
+//Soluzione:
+
+function dataDiNascita (anno:number):string {
+    if (anno_nascita<anno_corrente) {
+        console.log("Hai "+risultato+ " anni");
+    }
+}
+
+let anno_corrente:number=2022;
+let anno_nascita:number=prompt("Inserisci anno di nascita");
+let risultato=(anno_corrente - anno_nascita);
+dataDiNascita(risultato);

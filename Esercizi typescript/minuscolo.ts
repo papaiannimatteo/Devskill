b1ebdd2f5960139408498a629423cf0cc8539f94
Matteo Papaianni <papaiannimatteo746@gmail.com>
2022-04-21 12:09:46 +0200

    questi sono gli esercizi del maiuscolo e del minuscolo

---
+++ Esercizi typescript/minuscolo.ts
@@ -0,0 +1,8 @@
+let nomeCognome:string=prompt("Inserisci il tuo nome e cognome");
+
+function maiuscolo(nomCognome:string):void {
+    console.log(nomeCognome.toLowerCase());
+    
+}
+
+maiuscolo(nomeCognome);
